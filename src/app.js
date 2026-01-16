572bb7ef2f04f09da2edc7a891b4cca6bf3b26f0
shifan60 <shifankichu200@gmail.com>
2026-01-16 18:36:53 +0530

    Lastpush

--- src/app.js
+++ src/app.js
@@ -7,35 +7,9 @@ import taskRoutes from "./routes/taskRoutes.js"
 import crypto from "crypto"
 
 const app = express()
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", 
-    credentials: true,               
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
-app.use(morgan())
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRoutes)
-app.use("/api/tasks",taskRoutes)
-// axios.get(`${import.meta.env.VITE_API_URL}/api/tasks`)
-axios.post(`${API}/login`, data, {
-  withCredentials: true
-});
 const API = import.meta.env.VITE_API_URL;
-//Generate secret key
-// const key = crypto.randomBytes(64).toString("hex")
-// console.log(key)
-
-app.get("/",(req,res)=>{
-    res.send("<h1>helloworld</h1>")
-})
 app.use(cors({
   origin: [
-    "http://localhost:5173",
     "https://todo-management-frontend-ecru.vercel.app"
   ],
   credentials: true
@@ -47,4 +21,24 @@ res.cookie("token", token, {
 });
 
 
+app.use(morgan())
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRoutes)
+app.use("/api/tasks",taskRoutes)
+// axios.get(`${import.meta.env.VITE_API_URL}/api/tasks`)
+axios.post(`${API}/login`, data, {
+  withCredentials: true
+});
+
+//Generate secret key
+// const key = crypto.randomBytes(64).toString("hex")
+// console.log(key)
+
+app.get("/",(req,res)=>{
+    res.send("<h1>helloworld</h1>")
+})
+
+
 export default app
+
