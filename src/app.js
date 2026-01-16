eb3214a1841f306c30a1f695ee86ee5629b055a2
shifan60 <shifankichu200@gmail.com>
2026-01-16 20:17:42 +0530

    lastcommit

--- src/app.js
+++ src/app.js
@@ -7,24 +7,16 @@ import taskRoutes from "./routes/taskRoutes.js"
 import crypto from "crypto"
 
 const app = express()
-const API = import.meta.env.VITE_API_URL;
-
 app.use(cors({
   origin: '"https://todo-management-frontend-ecru.vercel.ap', // allow your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // if using cookies or authentication headers
 }));
-
 app.use(morgan())
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/tasks",taskRoutes)
-// axios.get(`${import.meta.env.VITE_API_URL}/api/tasks`)
-axios.post(`${API}/login`, data, {
-  withCredentials: true
-});
-
 //Generate secret key
 // const key = crypto.randomBytes(64).toString("hex")
 // console.log(key)
