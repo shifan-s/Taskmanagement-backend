092d65231d6cf533b6dc1d17e0b59edcf39ac645
shifan60 <shifankichu200@gmail.com>
2026-01-17 10:57:19 +0530

    Task

--- src/app.js
+++ src/app.js
@@ -7,14 +7,16 @@ import taskRoutes from "./routes/taskRoutes.js"
 import crypto from "crypto"
 
 const app = express()
-app.use(cors({
-  origin: [
-    "https://to-d-management.netlify.app/"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://to-d-management.netlify.app"
+    ],
+    credentials: true
+  })
+);
+app.options("*", cors());
 app.use(morgan())
 app.use(express.json())
 app.use(cookieParser())
