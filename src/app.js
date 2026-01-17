8bde19700f2990f1a45208767054b833f6b7ecae
shifan60 <shifankichu200@gmail.com>
2026-01-17 12:13:33 +0530

    change

--- src/app.js
+++ src/app.js
@@ -7,15 +7,10 @@ import taskRoutes from "./routes/taskRoutes.js"
 import crypto from "crypto"
 
 const app = express()
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://to-d-management.netlify.app"
-    ],
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: "https://to-do-management.netlify.app",
+  credentials: true
+}));
 app.options("*", cors());
 app.use(morgan())
 app.use(express.json())
